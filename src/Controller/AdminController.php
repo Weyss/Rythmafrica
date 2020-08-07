491eb7ed3904fda1e154aac4693c715a670c0d65
Weyss <ji.cedric06@gmail.com>
2020-08-07 23:32:37 +0200

    Creation des vues Listes pour le Back

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -8,12 +8,44 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/admin", name="index")
      */
     public function index()
     {
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
         ]);
+    }
+
+    /**
+     * @Route("/admin/artists", name="artists")
+     */
+    public function artists()
+    {
+        return $this->render('admin/artists.html.twig');
+    }
+
+    /**
+     * @Route("/admin/musics", name="musics")
+     */
+    public function musics()
+    {
+        return $this->render('admin/musics.html.twig');
+    }
+
+    /**
+     * @Route("/admin/events", name="events")
+     */
+    public function events()
+    {
+        return $this->render('admin/events.html.twig');
+    }
+
+    /**
+     * @Route("/admin/categories", name="categories")
+     */
+    public function categories()
+    {
+        return $this->render('admin/categories.html.twig');
     }
 }
